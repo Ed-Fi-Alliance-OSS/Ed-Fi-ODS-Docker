542702b29564e4a9f0e730a3293d75b338ba7a8b
dalluinniveinc <72984036+dalluinniveinc@users.noreply.github.com>
2020-10-26 12:04:36 +0530

    updated with passwordfile

--- PSQL-ODS/docker-entrypoint.sh
+++ PSQL-ODS/docker-entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-# Before PostgreSQL can function correctly, the database cluster must be initialized:
 initdb -D /var/lib/postgres/data
 pg_ctl -D "/var/lib/postgres/data" -o "-c listen_addresses=''" -w start
-# create a user or role
-psql -d postgres -c "CREATE USER pguser WITH PASSWORD 'password@';"
-# create database
+source ./db_auth.cfg
+psql -d postgres -c "CREATE USER pguser WITH PASSWORD=$(eval echo ${DB_PASSWORD} | base64 --decode);"
 psql -v ON_ERROR_STOP=1 -d postgres -c "CREATE DATABASE dockertest OWNER 'pguser';"
-# stop internal postgres server
 pg_ctl -v ON_ERROR_STOP=1 -D "/var/lib/postgres/data" -m fast -w stop
 exec "$@"
