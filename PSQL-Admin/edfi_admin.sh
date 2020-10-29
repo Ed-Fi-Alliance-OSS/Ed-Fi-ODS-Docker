ee6be115f22ebff7789a356fb14cf4d9334cae1a
ptar-innive <ptar@inniveinc.com>
2020-10-29 15:26:05 +0100

    lowering uppercase letters in dbname

--- PSQL-Admin/edfi_admin.sh
+++ PSQL-Admin/edfi_admin.sh
@@ -1,11 +1,11 @@
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE EdFi_Admin;
-    GRANT ALL PRIVILEGES ON DATABASE EdFi_Admin TO $POSTGRES_USER;
+    CREATE DATABASE edfi_admin;
+    GRANT ALL PRIVILEGES ON DATABASE edfi_admin TO $POSTGRES_USER;
 EOSQL
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "EdFi_Admin" < /app/EdFi_Admin.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_admin" < /app/EdFi_Admin.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE EdFi_Security;
-    GRANT ALL PRIVILEGES ON DATABASE EdFi_Security TO $POSTGRES_USER;
+    CREATE DATABASE edfi_security;
+    GRANT ALL PRIVILEGES ON DATABASE edfi_security TO $POSTGRES_USER;
 EOSQL
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "EdFi_Security" < /app/EdFi_Security.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_security" < /app/EdFi_Security.sql
--- PSQL-ODS/edfi_ods.sh
+++ PSQL-ODS/edfi_ods.sh
@@ -1,11 +1,11 @@
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE EdFi_Ods_Minimal_Template;
-    GRANT ALL PRIVILEGES ON DATABASE EdFi_Ods_Minimal_Template TO $POSTGRES_USER;
+    CREATE DATABASE edfi_ods_minimal_template;
+    GRANT ALL PRIVILEGES ON DATABASE edfi_ods_minimal_template TO $POSTGRES_USER;
 EOSQL
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "EdFi_Ods_Minimal_Template" < /app/EdFi.Ods.Minimal.Template.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_ods_minimal_template" < /app/EdFi.Ods.Minimal.Template.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE EdFi_Ods_Populated_Template;
-    GRANT ALL PRIVILEGES ON DATABASE EdFi_Ods_Populated_Template TO $POSTGRES_USER;
+    CREATE DATABASE edfi_ods_populated_template;
+    GRANT ALL PRIVILEGES ON DATABASE edfi_ods_populated_template TO $POSTGRES_USER;
 EOSQL
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "EdFi_Ods_Populated_Template" < /app/EdFi.Ods.Populated.Template.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_ods_populated_template" < /app/EdFi.Ods.Populated.Template.sql
