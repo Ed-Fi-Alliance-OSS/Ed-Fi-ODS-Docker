cd777a479a0a160e479f6d3e72762a9f363dae31
dalluinniveinc <72984036+dalluinniveinc@users.noreply.github.com>
2020-10-29 17:30:58 +0530

    adding .sh files

---
+++ PSQL-Admin/edfi_admin.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 --username 'postgres' <<-EOSQL
+    #CREATE USER postgres with superuser password 'pgpassword';
+    CREATE DATABASE EdFi_Admin;
+    GRANT ALL PRIVILEGES ON DATABASE EdFi_Admin TO postgres;
+EOSQL
