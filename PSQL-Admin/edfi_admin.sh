a04be65b10cafced068e45c3ca71a508c87f3e47
ptar-innive <ptar@inniveinc.com>
2020-10-29 16:16:25 +0100

    hardcoding some postgres as in the sqldumps

--- PSQL-Admin/edfi_admin.sh
+++ PSQL-Admin/edfi_admin.sh
@@ -1,10 +1,10 @@
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE edfi_admin;
-    GRANT ALL PRIVILEGES ON DATABASE edfi_admin TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE edfi_admin TO postgres;
 EOSQL
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_admin" < /app/EdFi_Admin.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE edfi_security;
-    GRANT ALL PRIVILEGES ON DATABASE edfi_security TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE edfi_security TO postgres;
 EOSQL
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_security" < /app/EdFi_Security.sql
--- PSQL-ODS/edfi_ods.sh
+++ PSQL-ODS/edfi_ods.sh
@@ -1,10 +1,10 @@
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE edfi_ods_minimal_template;
-    GRANT ALL PRIVILEGES ON DATABASE edfi_ods_minimal_template TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE edfi_ods_minimal_template TO postgres;
 EOSQL
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_ods_minimal_template" < /app/EdFi.Ods.Minimal.Template.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE edfi_ods_populated_template;
-    GRANT ALL PRIVILEGES ON DATABASE edfi_ods_populated_template TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE edfi_ods_populated_template TO postgres;
 EOSQL
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_ods_populated_template" < /app/EdFi.Ods.Populated.Template.sql
