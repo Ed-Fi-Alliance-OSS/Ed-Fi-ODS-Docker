e155b709db338158bcaa91f78cb848f844d8ad60
Jim McKay <jmckay@certicasolutions.com>
2020-11-01 15:43:37 -0600

    updated settings file and scripts

--- PSQL-Admin/edfi_admin.sh
+++ PSQL-Admin/edfi_admin.sh
@@ -1,10 +1,13 @@
+export PGCLIENTENCODING="utf-8"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE edfi_admin;
-    GRANT ALL PRIVILEGES ON DATABASE edfi_admin TO postgres;
+    CREATE DATABASE \"EdFi_Admin\";
+    GRANT ALL PRIVILEGES ON DATABASE \"EdFi_Admin\" TO postgres;
 EOSQL
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_admin" < /app/EdFi_Admin.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "Edfi_Admin" < /app/EdFi_Admin.sql
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE edfi_security;
-    GRANT ALL PRIVILEGES ON DATABASE edfi_security TO postgres;
+    CREATE DATABASE \"EdFi_Security\";
+    GRANT ALL PRIVILEGES ON DATABASE \"EdFi_Security\" TO postgres;
 EOSQL
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_security" < /app/EdFi_Security.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "EdFi_Security" < /app/EdFi_Security.sql
--- PSQL-ODS/edfi_ods.sh
+++ PSQL-ODS/edfi_ods.sh
@@ -1,10 +1,41 @@
+export PGCLIENTENCODING="utf-8"
+
+# Setup the minimal template
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE edfi_ods_minimal_template;
-    GRANT ALL PRIVILEGES ON DATABASE edfi_ods_minimal_template TO postgres;
+    CREATE DATABASE \"EdFi_Ods_Minimal_Template\";
+    GRANT ALL PRIVILEGES ON DATABASE \"EdFi_Ods_Minimal_Template\" TO postgres;
+
 EOSQL
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_ods_minimal_template" < /app/EdFi.Ods.Minimal.Template.sql
+
+# Load the minimal template
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "EdFi_Ods_Minimal_Template" < /app/EdFi.Ods.Minimal.Template.sql
+
+# Mark the miminmal template as a template database
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE edfi_ods_populated_template;
-    GRANT ALL PRIVILEGES ON DATABASE edfi_ods_populated_template TO postgres;
+    SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='EdFi_Ods_Minimal_Template';
+    UPDATE pg_database SET datistemplate='true', datallowconn='false' WHERE datname in ('EdFi_Ods_Minimal_Template');
+
 EOSQL
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "edfi_ods_populated_template" < /app/EdFi.Ods.Populated.Template.sql
+
+# Setup the populated template
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE \"Edfi_Ods_Populated_Template\";
+    GRANT ALL PRIVILEGES ON DATABASE \"EdFi_Ods_Populated_Template\" TO postgres;
+
+EOSQL
+
+# Load the populated template
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "EdFi_Ods_Populated_Template" < /app/EdFi.Ods.Populated.Template.sql
+
+# Mark the populated template as a template database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='EdFi_Ods_Populated_Template';
+    UPDATE pg_database SET datistemplate='true', datallowconn='false' WHERE datname in ('EdFi_Ods_Populated_Template');
+
+EOSQL
+
+# Create a shared instance databse for production use
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE \"EdFi_Ods" TEMPLATE \"EdFi_Ods_MinimalTemplate\";
+
+EOSQL
