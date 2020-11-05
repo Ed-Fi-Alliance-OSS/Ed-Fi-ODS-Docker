c0d0f572a37f70749af8d9d5fb69418d7299e7e4
Jim McKay <jmckay@certicasolutions.com>
2020-11-05 13:11:13 -0600

    updated docker file for the ods database

---
+++ PSQL-ODS/init-database.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "EdFi_Ods_Populated_Template" TEMPLATE template0;
+    GRANT ALL PRIVILEGES ON DATABASE "EdFi_Ods_Populated_Template" TO postgres;
+    CREATE DATABASE "EdFi_Ods_Minimal_Template" TEMPLATE template0;
+    GRANT ALL PRIVILEGES ON DATABASE "EdFi_Ods_Minimal_Template" TO postgres;
+EOSQL
+
+psql -v --no-password --tuples-only --username postgres --dbname "EdFi_Ods_Minimal_Template" --file /tmp/EdFi_Ods_Minimal_Template.sql
+
+psql -v --no-password --tuples-only --username postgres --dbname "EdFi_Ods_Populated_Template" --file /tmp/EdFi_Ods_Populated_Template.sql
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    UPDATE pg_database SET datistemplate='true', datallowconn='false' WHERE datname in ('EdFi_Ods_Populated_Template', 'EdFi_Ods_Minimal_Template');
+EOSQL
