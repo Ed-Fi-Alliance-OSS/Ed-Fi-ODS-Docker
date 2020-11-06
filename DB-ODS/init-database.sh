c873694d9463e2a352ed4153715b3d929fe2e686
Jim McKay <jmckay@certicasolutions.com>
2020-11-06 09:47:48 -0600

    updated the license headers, minor cleanup

--- DB-ODS/init-database.sh
+++ DB-ODS/init-database.sh
@@ -1,11 +1,15 @@
-
 #!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE "EdFi_Ods_Populated_Template" TEMPLATE template0;
-    GRANT ALL PRIVILEGES ON DATABASE "EdFi_Ods_Populated_Template" TO postgres;
     CREATE DATABASE "EdFi_Ods_Minimal_Template" TEMPLATE template0;
+    GRANT ALL PRIVILEGES ON DATABASE "EdFi_Ods_Populated_Template" TO postgres;
     GRANT ALL PRIVILEGES ON DATABASE "EdFi_Ods_Minimal_Template" TO postgres;
 EOSQL
 
@@ -14,4 +18,7 @@ psql -v --no-password --tuples-only --username postgres --dbname "EdFi_Ods_Minim
 psql -v --no-password --tuples-only --username postgres --dbname "EdFi_Ods_Populated_Template" --file /app/EdFi_Ods_Populated_Template.sql
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-UPDATE pg_database SET datistemplate='true', datallowconn='false' WHERE datname in ('EdFi_Ods_Populated_Template', 'EdFi_Ods_Minimal_Template');
+    UPDATE pg_database SET datistemplate='true', datallowconn='false' WHERE datname in ('EdFi_Ods_Populated_Template', 'EdFi_Ods_Minimal_Template');
+    CREATE DATABASE "EdFi_ODS" TEMPLATE 'EdFi_Ods_Minimal_Template';
+    GRANT ALL PRIVILEGES ON DATABASE "EdFi_Ods" TO postgres;
+EOSQL
--- DB-Sandbox/init-database.sh
+++ DB-Sandbox/init-database.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
 set -e
 
 psql ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
