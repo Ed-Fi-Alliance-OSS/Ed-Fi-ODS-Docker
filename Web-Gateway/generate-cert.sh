ef12266fe5e25bb2b37d6957053e90b0e842e4e0
Thomas Nunnery <t.nunnery@wearedoubleline.com>
2020-11-11 09:57:52 -0600

    Changed all references of GateWay to Gateway.

--- Web-Gateway/generate-cert.sh
+++ Web-Gateway/generate-cert.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ `basename "${PWD}"` == "GateWay" ]];
+if [[ `basename "${PWD}"` == "Gateway" ]];
 then
     mkdir -p ssl
     openssl dhparam -out ssl/dhparam.pem 4096
     openssl req -x509 -newkey rsa:4096 -nodes -keyout ssl/server.key -out ssl/server.crt -days 365
 else
-    echo "NOT in GateWay folder"
+    echo "NOT in Gateway folder"
     exit -1
 fi
