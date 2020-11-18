3562264c8656a036b8276a5fa8d5d7aaf8eee45e
Thomas Nunnery <t.nunnery@wearedoubleline.com>
2020-11-18 12:51:26 -0600

    Added HTTPS support to reverse proxy.

--- Web-Gateway/generate-cert.sh
+++ Web-Gateway/generate-cert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ `basename "${PWD}"` == "Gateway" ]];
+if [[ `basename "${PWD}"` == "Web-Gateway" ]];
 then
     mkdir -p ssl
     openssl dhparam -out ssl/dhparam.pem 4096
-    openssl req -x509 -newkey rsa:4096 -nodes -keyout ssl/server.key -out ssl/server.crt -days 365
+    openssl req -subj '/CN=localhost' -x509 -newkey rsa:4096 -nodes -keyout ssl/server.key -out ssl/server.crt -days 365
 else
     echo "NOT in Gateway folder"
     exit -1
