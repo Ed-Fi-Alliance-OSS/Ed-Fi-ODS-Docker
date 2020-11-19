af544718d7fc6d134bf74b13ce987f85182b000e
smundheinnive <72035812+smundheinnive@users.noreply.github.com>
2020-11-19 22:26:17 +0530

    Update init-database.sh

--- DB-ODS/init-database.sh
+++ DB-ODS/init-database.sh
@@ -13,9 +13,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE "EdFi_Ods_Minimal_Template" TO $POSTGRES_USER;
 EOSQL
 
-psql -v --no-password --tuples-only --username "$POSTGRES_USER" --dbname "EdFi_Ods_Minimal_Template" --file /app/EdFi_Ods_Minimal_Template.sql
+psql -v --no-password --tuples-only --username "$POSTGRES_USER" --dbname "EdFi_Ods_Minimal_Template" --file /tmp/EdFi_Ods_Minimal_Template.sql
 
-psql -v --no-password --tuples-only --username "$POSTGRES_USER" --dbname "EdFi_Ods_Populated_Template" --file /app/EdFi_Ods_Populated_Template.sql
+psql -v --no-password --tuples-only --username "$POSTGRES_USER" --dbname "EdFi_Ods_Populated_Template" --file /tmp/EdFi_Ods_Populated_Template.sql
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     UPDATE pg_database SET datistemplate='true', datallowconn='false' WHERE datname in ('EdFi_Ods_Populated_Template', 'EdFi_Ods_Minimal_Template');
