78b34563687bc276dbf2a667dfc49043752161e1
Jim McKay <jmckay@certicasolutions.com>
2020-12-02 09:47:48 -0600

    code review changes

--- sandbox-env-clean.ps1
+++ sandbox-env-clean.ps1
@@ -3,7 +3,7 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-docker-compose -f .\compose-sandbox-env.yml down -v --remove-orphans
+docker-compose -f .\compose-sandbox-env-build.yml down -v --remove-orphans
 
 docker rmi ed-fi-ods-docker_api ed-fi-ods-docker_swagger ed-fi-swagger ed-fi-ods-docker_nginx ed-fi-ods-docker_admin  -f
 
--- sandbox-env-up.ps1
+++ sandbox-env-up.ps1
@@ -3,4 +3,4 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-docker-compose -f .\compose-sandbox-env.yml up -d
+docker-compose -f .\compose-sandbox-env-build.yml up -d
