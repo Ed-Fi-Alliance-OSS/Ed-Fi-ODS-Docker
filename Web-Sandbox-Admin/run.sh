6a5aeaed3a40859855381ecf7aeff1cb52338003
Jim McKay <jmckay@certicasolutions.com>
2020-12-02 14:34:18 -0600

    reduced the timeout

--- Web-Sandbox-Admin/run.sh
+++ Web-Sandbox-Admin/run.sh
@@ -9,7 +9,7 @@ envsubst < /app/appsettings.template.json > /app/appsettings.json
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h $ODS_DB -U $POSTGRES_USER -c '\q'; 
 do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 30
+  sleep 10
 done
   
 >&2 echo "Postgres is up - executing command"
