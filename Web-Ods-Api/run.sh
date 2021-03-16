9bed09ccf8d74c74ffc661fe6fd4fbc151016262
Jim McKay <james.mckay@instructure.com>
2021-03-16 08:42:11 -0500

    sample compose file for multiple databases

--- Web-Ods-Api/run.sh
+++ Web-Ods-Api/run.sh
@@ -6,12 +6,24 @@
 
 envsubst < /app/appsettings.template.json > /app/appsettings.json
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $ODS_DB -U $POSTGRES_USER -c '\q'; 
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h $ODS_DB -U $POSTGRES_USER -c '\q';
 do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 10
 done
-  
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h $ODS_DB-2020 -U $POSTGRES_USER -c '\q';
+do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 10
+done
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h $ODS_DB-2021 -U $POSTGRES_USER -c '\q';
+do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 10
+done
+
 >&2 echo "Postgres is up - executing command"
 exec $cmd
 
