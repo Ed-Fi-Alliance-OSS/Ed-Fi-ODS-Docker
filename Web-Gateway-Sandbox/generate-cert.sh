afe909e37c84c97d8efaf3aa55e425b8fdc26fcf
Jim McKay <jamessmckay@users.noreply.github.com>
2021-03-22 16:48:24 -0500

    [ODS-4766] Echo database init script commands (#55)
    * added verbose to all bash scripts
    
    * switched to using echo instead so that all variables are resolved.
    
    * code review changes

--- Web-Gateway-Sandbox/generate-cert.sh
+++ Web-Gateway-Sandbox/generate-cert.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+set -e
+set -x
 
 if [[ `basename "${PWD}"` == "Web-Gateway-Sandbox" ]];
 then
--- Web-Sandbox-Admin/run.sh
+++ Web-Sandbox-Admin/run.sh
@@ -4,14 +4,17 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+set -e
+set -x
+
 envsubst < /app/appsettings.template.json > /app/appsettings.json
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $ODS_DB -U $POSTGRES_USER -c '\q'; 
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h $ODS_DB -U $POSTGRES_USER -c '\q';
 do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 10
 done
-  
+
 >&2 echo "Postgres is up - executing command"
 exec $cmd
 
