7a3b16286380a416a3e5331c7d78370a6c38e75e
Axel Marquez <axel_marqz@hotmail.com>
2022-02-16 11:43:53 -0600

    Disable TPDM extension based on TPDM_ENABLED parameter

--- Web-Ods-Api/Alpine/mssql/run.sh
+++ Web-Ods-Api/Alpine/mssql/run.sh
@@ -4,6 +4,13 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+set -e
+set -x
+
+if [[ "$TPDM_ENABLED" != true ]]; then
+    export Plugin__Folder="./Plugin_Disabled"
+fi
+
 envsubst < /app/appsettings.template.json > /app/appsettings.json
 
 dotnet EdFi.Ods.WebApi.dll
