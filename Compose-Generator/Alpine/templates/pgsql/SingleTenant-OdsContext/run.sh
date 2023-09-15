a8814bd8fcf8bb516966e7f57ae11c2e1fc1be0b
Jesus Flores <43448209+simpat-jesus@users.noreply.github.com>
2023-09-15 17:13:01 -0600

    [ODS-5997] Add SingleTenant with Context routes (school years) example (#162)
    * Add compose templates for single tenant with ods context
    
    * Add compose generated example for single tenant with ods context

---
+++ Compose-Generator/Alpine/templates/pgsql/SingleTenant-OdsContext/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+/app/mustache parameters /app/singleTenant-odsContext/appsettings-template.mustache > output/appsettings.docker.json
+/app/mustache parameters /app/singleTenant-odsContext/bootstrap-template.mustache > output/bootstrap.sh
+/app/mustache parameters /app/singleTenant-odsContext/compose-template.mustache > output/compose.yml
